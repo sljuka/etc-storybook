353497e842a96673c2b970952a70ccaba776e5e6
David Sljukic <david.sljukic.dev@gmail.com>
2019-11-21 15:43:13 +0100

    Remove commented flow diagram story

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -1,21 +1,5 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-// import { Diagram } from "../components/flowDiagram";
-// import {
-//   ActivityNode,
-//   ActivityNodePort
-// } from "../components/flowDiagram/Nodes/Activity/ActivityNode";
-// import { oldTheme as theme } from "../components/flowDiagram/theme";
-// import { GrayLabel } from "../components/GrayLabel";
-// import {
-//   EventNodePort,
-//   EventNode
-// } from "../components/flowDiagram/Nodes/Event/EventNode";
-// import {
-//   GatewayPort,
-//   GatewayNode,
-//   Spacer
-// } from "../components/flowDiagram/Nodes/Gateway/GatewayNode";
 import { CustomThemeTable } from "../components/table";
 
 storiesOf("Grommet component lib", module).add(
