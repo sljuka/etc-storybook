35243174750456b2180713385215322fe1e9a93d
David Sljukic <david.sljukic.dev@gmail.com>
2019-11-21 23:36:55 +0100

    Add material-ui table

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { CustomThemeTable } from "../components/table";
+import { EnhancedTable } from "../components/materialUI/Table";
 
 storiesOf("Grommet component lib", module).add(
   "Sample table",
   () => <CustomThemeTable />,
+  {
+    info: { inline: true }
+  }
+);
+
+storiesOf("Material UI component lib", module).add(
+  "Sample table",
+  () => <EnhancedTable />,
   {
     info: { inline: true }
   }
